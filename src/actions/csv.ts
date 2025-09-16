02176dc69ac96ae42e10493ccf790b3f50bd10af
Jordi Petit <jordi.petit@gmail.com>
2025-09-16 10:58:17 +0200

    Add action to parse CSV string to array of dicts

--- src/actions/csv.ts
+++ src/actions/csv.ts
@@ -1,8 +1,8 @@
 'use server'
-
 // because fast-cvs does not work on the browser, we need to use it on the server side
 
 import { writeToString } from '@fast-csv/format'
+import { parseString } from '@fast-csv/parse'
 
 export async function array2csv(data: any[]) {
     return await writeToString(data, {
@@ -10,4 +10,26 @@ export async function array2csv(data: any[]) {
         delimiter: ',',
         quoteColumns: true,
     })
+}
+
+export interface CsvRow {
+    [key: string]: string
+}
+
+function parse(csvString: string): Promise<CsvRow[]> {
+    return new Promise((resolve, reject) => {
+        const results: CsvRow[] = []
+
+        parseString(csvString, {
+            headers: true,
+            delimiter: ';',
+        })
+            .on('data', (row: CsvRow) => results.push(row))
+            .on('end', () => resolve(results))
+            .on('error', (error: Error) => reject(error))
+    })
+}
+
+export async function csv2array(csvString: string): Promise<CsvRow[]> {
+    return await parse(csvString)
 }
