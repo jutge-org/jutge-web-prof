8a1b080751872bb7ff723df7773c6c5be74d8823
Jordi Petit <jordi.petit@gmail.com>
2025-12-04 09:16:16 +0100

    Upgrade NextJs because of some vulnerability

--- next.config.ts
+++ next.config.ts
@@ -13,12 +13,6 @@ module.exports = {
         },
     },
 
-    eslint: {
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-        //        ignoreDuringBuilds: true,
-    },
-
     images: {
         remotePatterns: [
             {
