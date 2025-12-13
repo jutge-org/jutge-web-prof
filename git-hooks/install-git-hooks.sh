1e3ab564d630e539073afeecd061042e7fc22b5f
Jordi Petit <jordi.petit@gmail.com>
2025-12-13 11:02:24 +0100

    Remove git hooks

--- git-hooks/install-git-hooks.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SOURCE_DIR=../../src/git-hooks # from .git/hooks
-HOOK_DIR=$(git rev-parse --show-toplevel)/.git/hooks
-
-# WARNING: This will replace any pre-commit hooks
-ln -s -f $SOURCE_DIR/git-hook-format-files.sh $HOOK_DIR/pre-commit
