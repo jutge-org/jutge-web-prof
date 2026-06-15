f716f46e1183dd42156126a0314db8f37b7aa073
Jordi Petit <jordi.petit@gmail.com>
2026-06-15 16:07:56 +0200

    Update AI prices

--- lib/ai-utils.ts
+++ lib/ai-utils.ts
@@ -2,20 +2,20 @@ import { memo } from 'radash'
 
 
 // prices in USD per 1M tokens
-// data from https://openai.com/pricing/ and https://google.com/gemini/pricing/ at 2026-02-09
 const prices = {
-    'openai/gpt-5': {
-        input: 1.25,
-        output: 10.0,
+    'openai/gpt-5.4': {
+        input: 2.5,
+        output: 15.0,
     },
-    'openai/gpt-5-mini': {
-        input: 0.25,
-        output: 2.0,
+    'openai/gpt-5.4-mini': {
+        input: 0.75,
+        output: 4.5,
     },
-    'openai/gpt-5-nano': {
-        input: 0.05,
-        output: 0.4,
+    'openai/gpt-5.4-nano': {
+        input: 0.2,
+        output: 1.25,
     },
+
     'openai/gpt-4.1': {
         input: 2.0,
         output: 8.0,
@@ -28,6 +28,20 @@ const prices = {
         input: 0.1,
         output: 0.4,
     },
+
+    'openai/gpt-5': {
+        input: 1.25,
+        output: 10.0,
+    },
+    'openai/gpt-5-mini': {
+        input: 0.25,
+        output: 2.0,
+    },
+    'openai/gpt-5-nano': {
+        input: 0.05,
+        output: 0.4,
+    },
+
     'google/gemini-2.5-pro': {
         input: 1.25,
         output: 10.0,
@@ -40,7 +54,7 @@ const prices = {
         input: 0.1,
         output: 0.4,
     },
-}
+};
 
 function calculateCost(inputTokens: number, outputTokens: number, model: string) {
     if (!Object.keys(prices).includes(model)) {
